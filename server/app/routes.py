b690994459f6fb30140486aa60f14275ad127544
Beerkat <ad4mn4hif@gmail.com>
2025-06-05 11:14:53 +0100

    Implement the image prediction api

--- server/app/routes.py
+++ server/app/routes.py
@@ -1,7 +1,21 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, request, send_file
+from PIL import Image
+from .inference import predict
 
 main_bp = Blueprint('main', __name__)
 
-@main_bp.route("/ping", methods=["GET"])
-def ping():
-    return jsonify(message="Pong! Flask is running.")
+# @main_bp.route('/', methods=['GET'])
+# def upload_image():
+#     return """
+#         <form action="" method="post" enctype="multipart/form-data">
+#             <input type="file" name="mammogram" id="mammogram" accept="image/*" required/>
+#             <input type="submit" />
+#         </form>
+#     """ 
+
+@main_bp.route("/", methods=["POST"])
+def receive_image():
+    mammogram_file = request.files['mammogram']
+    mammogram_image = Image.open(mammogram_file.stream).convert('L')
+    prediction_path = predict(mammogram_image) 
+    return send_file(prediction_path, mimetype='image/png')
